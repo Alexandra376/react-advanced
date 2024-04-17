7127cdb9ba7bf6de28e0e87f91936dbff535a9d7
Alexandra376 <suvorovaalexandra344@gmail.com>
2024-04-18 00:51:07 +0300

    feat: add component profile-card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
-import Lessons_02 from "./lessons/Lessons_02/Lessons_02";
+import Homework_02 from "./homeworks/Homework_02/Homework_02";
+// import Lessons_02 from "./lessons/Lessons_02/Lessons_02";
 
 function App() {
   return (
       <div className="App">
-          <Lessons_02/>
+          {/*<Lessons_02/>*/}
+          <Homework_02/>
       </div>
   );
 }
---
+++ src/homeworks/Homework_02/Homework_02.jsx
@@ -0,0 +1,14 @@
+import "./Homework_02.css"
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+
+function Homework_02() {
+    return(
+        <div className = "cards-block">
+            <ProfileCard/>
+            <ProfileCard/>
+            <ProfileCard/>
+        </div>
+    )
+}
+
+export default Homework_02
