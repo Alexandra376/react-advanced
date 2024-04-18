392b1a97dd3d53ca78de2bb2edf297c42bfdfc03
Alexandra376 <suvorovaalexandra344@gmail.com>
2024-04-18 19:04:25 +0300

    feat: add new component textArea

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
+import Consultation_01 from "./consultation/Consultation_01/Consultation_01";
 // import Homework_02 from "./homeworks/Homework_02/Homework_02";
 // import Lessons_02 from "./lessons/Lessons_02/Lessons_02";
-import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
+// import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
 
 function App() {
   return (
       <div className="App">
           {/*<Lessons_02/>*/}
           {/*<Homework_02/>*/}
-          <Lesson_03/>
+          {/*<Lesson_03/>*/}
+          <Consultation_01/>
       </div>
   );
 }
